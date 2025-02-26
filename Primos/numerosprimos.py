a4cee37a90de464380f707871f9096629f267598
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-02-26 13:47:39 -0300

    Create numerosprimos.py

---
+++ Primos/numerosprimos.py
@@ -0,0 +1,15 @@
+cont = 0
+lista = []
+conta = 0
+n = int(input('Quantos números primos você quer ver?  '))
+while True:
+    cont += 1
+    for a in range(1, cont):
+        if cont % a == 0:
+            conta += 1
+    if conta == 1:
+        lista.append(cont)
+    conta = 0
+    if len(lista) == n:
+        break
+print(lista)
