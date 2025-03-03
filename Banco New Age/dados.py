790d78f1596da106b0ad355f338dbc3e25f99518
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-03-03 17:18:37 -0300

    vamos mudar

--- Banco New Age/dados.py
+++ Banco New Age/dados.py
@@ -1,13 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import sqlite3
+from flask import Flask
+    
 app = Flask(__name__)
-def conectar_db():
-    banco = sqlite3.connect('Banco New Age/contas.db')
-    return banco
-def criar_tabela():
-    banco = conectar_db()
-    cursor = banco.cursor()
-    cursor.execute('''
-            CREATE TABLE IF NOT EXISTS dados (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-                   nome TEXT NOT NULL,
-                   senha TEXT NOT NULL)''')
+
